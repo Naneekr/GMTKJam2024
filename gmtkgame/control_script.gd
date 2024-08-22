bcfe70975c7cb509886e17303d9ca4a98d632966
TheLostLight <seemydolphin@gmail.com>
2024-08-22 05:25:43 -0700

    Added top bar. Added hour counter. Added day counter

--- gmtkgame/control_script.gd
+++ gmtkgame/control_script.gd
@@ -1,12 +1,20 @@
 extends Control
 
-var num_of_clients: int = 2
+const LAST_HOUR: int = 8
+const num_of_clients: int = 2
+
+var day_count: int = 1
+var hour_count: int = 1
+var hour_label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$BoxContainer/MarginContainer/AspectRatioContainer/ClientButton.pressed.connect(self._take_client)
-	$BoxContainer/MarginContainer2/AspectRatioContainer2/SmithButton.pressed.connect(self._smith_item)
-	$BoxContainer/MarginContainer3/AspectRatioContainer3/MapButton.pressed.connect(self._go_to_map)
+	var tab_1 = $VBoxContainer/TabContainer/BoxContainer
+	tab_1.get_node("MarginContainer/AspectRatioContainer/ClientButton").pressed.connect(self._take_client)
+	tab_1.get_node("MarginContainer2/AspectRatioContainer2/SmithButton").pressed.connect(self._smith_item)
+	tab_1.get_node("MarginContainer3/AspectRatioContainer3/MapButton").pressed.connect(self._go_to_map)
+	
+	hour_label = $VBoxContainer/HBoxContainer/HourCountLabel
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +22,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _take_client() -> void:
+	if not _increment_hour(1):
+		return #TODO: Add no time left message
+	
 	var rng = RandomNumberGenerator.new()
 	
 	var client_choice: int = rng.randi_range(1, num_of_clients)
@@ -25,3 +36,14 @@ func _smith_item() -> void:
 	
 func _go_to_map() -> void:
 	print("Away we go!")
+	
+func _increment_hour(delta: int) -> bool:
+	delta = hour_count + delta
+	
+	if delta <= LAST_HOUR:
+		hour_count = delta
+		hour_label.text = str(hour_count)
+		return true
+	
+	else:
+		return false
